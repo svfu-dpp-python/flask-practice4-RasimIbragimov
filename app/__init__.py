ac5754ccdf045ac19c7df1220755a6762129a520
RasimIbragimov <planet_saturn2001@mail.ru>
2024-04-16 17:14:09 +0900

    Login

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,10 @@ from . import views
 
 def create_app():
     app = Flask(__name__)
-
+    
     # Конфигурация
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+    app.config["SECRET_KEY"] = "secret"
 
     # База данных
     db.init_app(app)
@@ -16,5 +17,7 @@ def create_app():
 
     # Функции представления
     app.add_url_rule("/", view_func=views.index_page)
-
+    app.add_url_rule("/login/", view_func=views.login_page, methods=["GET", "POST"])
+    app.add_url_rule("/logout/", view_func=views.logout)
+    
     return app
